b7fc37598c63e1aab09accbd56eeebac069c8f1c
Eric <eric.audemard@umontreal.ca>
2019-07-18 18:00:53 -0400

    add create matrix tpm

--- run_init.sh
+++ run_init.sh
@@ -3,5 +3,8 @@
 Rscript --vanilla ./src/script/other/biotype.r
 Rscript --vanilla ./src/script/other/create_gene_length.r
 
+# read count to tpm
+Rscript --vanilla ./src/script/other/create_matrix_tpm.r
+
 #To build data/TCGA_BRCA
 Rscript --vanilla ./src/script/other/brca_matrix_readcounts.r
