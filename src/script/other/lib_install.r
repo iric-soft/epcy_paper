67d30a3f822e6db44727670a3b7f952984393e53
Eric Audemard <eric.audemard@umontreal.ca>
2019-07-19 13:28:36 -0400

    add dependency

--- src/script/other/lib_install.r
+++ src/script/other/lib_install.r
@@ -10,8 +10,10 @@ BiocManager::install("DESeq2", ask=F)
 BiocManager::install("edgeR", ask=F)
 BiocManager::install("limma", ask=F)
 BiocManager::install("biomaRt", ask=F)
+BiocManager::install("EnsDb.Hsapiens.v86", ask=F)
 
 ##Other
 
 install.packages("data.table")
 install.packages("dplyr")
+install.packages("here")
