96a0fecc71da60fabbfcc6079e0a79e4aa6d4706
eric audemard <eric.audemard@umontreal.ca>
2020-06-19 16:51:54 -0400

    add mast

---
+++ src/script/cmd/mast.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#mast.sh ${path_software} ${path_design} ${path_counts} ${path_output}
+
+path_software=$1
+path_design=$2
+path_counts=$3
+path_output=$4
+
+echo "Rscript --vanilla ${path_software} ${path_design} ${path_counts} ${path_output}"
