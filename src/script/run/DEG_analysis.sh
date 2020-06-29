60a2e874dc7592c64314e27a9fb4defc325bda04
eric audemard <eric.audemard@umontreal.ca>
2020-06-29 10:02:30 -0400

    adj torque ressouces for 10x_FACS

--- src/script/run/DEG_analysis.sh
+++ src/script/run/DEG_analysis.sh
@@ -26,11 +26,11 @@ mem_epcy="36Gb"
 walltime_edger="3:00:00"
 mem_edger="3Gb"
 
-walltime_limma="6:00:00"
-mem_limma="36Gb"
+walltime_limma="12:00:00"
+mem_limma="48Gb"
 
-walltime_mast="6:00:00"
-mem_mast="36Gb"
+walltime_mast="12:00:00"
+mem_mast="80Gb"
 
 walltime_LDE="12:00:00"
 mem_LDE="80Gb" # 2 * num_proc
