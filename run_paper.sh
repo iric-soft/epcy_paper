e3c7e46c0fd34c209addfe6b266b973815972927
eric audemard <eric.audemard@umontreal.ca>
2020-09-04 16:17:39 -0400

    40 proc for 10X_FACS

--- run_paper.sh
+++ run_paper.sh
@@ -76,7 +76,7 @@ data_project="leucegene3"
 #done
 
 
-num_proc="20"
+num_proc="40"
 data_type="sc"
 path_design="10X_FACS"
 data_project="10X_FACS"
@@ -91,6 +91,7 @@ for design in ${designs_10X}
   done
 done
 
+num_proc="20"
 data_type="sc"
 path_design="10X_FACS_reduce"
 data_project="10X_FACS_reduce"
