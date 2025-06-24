147102202948ea9bacdb96c0da33238679605524
eric audemard <ericaudemard@erics-MacBook-Pro.local>
2025-06-24 07:51:40 -0700

    test commit

--- run_init_paper.sh
+++ run_init_paper.sh
@@ -5,13 +5,20 @@ Rscript --vanilla ./src/script/other/gen_design_from_all.r ./data/design/leucege
 echo "Generate subgroup for 10X FACS"
 Rscript --vanilla ./src/script/other/gen_design_from_all.r ./data/design/10X_FACS/ subgroup
 echo "Generate subgroup for 10X FACS reduce"
-Rscript --vanilla ./src/script/other/gen_design_from_all.r ./data/design/10X_FACS_reduce/ subgroup
+Rscript --vanilla ./src/script/other/gen_10X_reduce.r
+for num_sample in  3000 5000 8000 10000; do
+  for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20; do
+    Rscript --vanilla ./src/script/other/gen_design_from_all.r ./data/design/10X_FACS_reduce_${num_sample}_$i/ subgroup
+  done
+done
 
 ## Create leucegene_random
 echo "Generate random for leucegene 3"
 Rscript --vanilla ./src/script/other/gen_leucegene_random.r
 echo "Generate random for 10X FACS reduce"
 Rscript --vanilla ./src/script/other/gen_10X_random.r
+Rscript --vanilla ./src/script/other/gen_10X_reduce_random.r
+Rscript --vanilla ./src/script/other/gen_10X_rep_random.r
 
 ## Create subsampling
 echo "Generate subsampling for leucegene3"
